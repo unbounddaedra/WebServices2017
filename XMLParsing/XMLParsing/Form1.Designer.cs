eb96342605e31f596030eb23df07e29dd38dee36
Button IV <wjbuttoniv@labs.neit.edu>
2017-10-05 20:07:15 -0400

    added xml parsing example

---
+++ XMLParsing/XMLParsing/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace XMLParsing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadXMLFile = new System.Windows.Forms.Button();
+            this.btnReadXMLValues = new System.Windows.Forms.Button();
+            this.btnInnerText = new System.Windows.Forms.Button();
+            this.btnFindingChildren = new System.Windows.Forms.Button();
+            this.btnGetElementByTagName = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadXMLFile
+            // 
+            this.btnReadXMLFile.Location = new System.Drawing.Point(37, 26);
+            this.btnReadXMLFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReadXMLFile.Name = "btnReadXMLFile";
+            this.btnReadXMLFile.Size = new System.Drawing.Size(111, 73);
+            this.btnReadXMLFile.TabIndex = 4;
+            this.btnReadXMLFile.Text = "Reading An XML File";
+            this.btnReadXMLFile.UseVisualStyleBackColor = true;
+            this.btnReadXMLFile.Click += new System.EventHandler(this.btnReadXMLFile_Click);
+            // 
+            // btnReadXMLValues
+            // 
+            this.btnReadXMLValues.Location = new System.Drawing.Point(196, 26);
+            this.btnReadXMLValues.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReadXMLValues.Name = "btnReadXMLValues";
+            this.btnReadXMLValues.Size = new System.Drawing.Size(111, 73);
+            this.btnReadXMLValues.TabIndex = 5;
+            this.btnReadXMLValues.Text = "Read XML Value";
+            this.btnReadXMLValues.UseVisualStyleBackColor = true;
+            this.btnReadXMLValues.Click += new System.EventHandler(this.btnReadXMLValues_Click);
+            // 
+            // btnInnerText
+            // 
+            this.btnInnerText.Location = new System.Drawing.Point(514, 26);
+            this.btnInnerText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInnerText.Name = "btnInnerText";
+            this.btnInnerText.Size = new System.Drawing.Size(111, 73);
+            this.btnInnerText.TabIndex = 6;
+            this.btnInnerText.Text = "Inner and Outer Text";
+            this.btnInnerText.UseVisualStyleBackColor = true;
+            this.btnInnerText.Click += new System.EventHandler(this.btnInnerText_Click);
+            // 
+            // btnFindingChildren
+            // 
+            this.btnFindingChildren.Location = new System.Drawing.Point(355, 26);
+            this.btnFindingChildren.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFindingChildren.Name = "btnFindingChildren";
+            this.btnFindingChildren.Size = new System.Drawing.Size(111, 73);
+            this.btnFindingChildren.TabIndex = 7;
+            this.btnFindingChildren.Text = "Finding Child Nodes";
+            this.btnFindingChildren.UseVisualStyleBackColor = true;
+            this.btnFindingChildren.Click += new System.EventHandler(this.btnFindingChildren_Click);
+            // 
+            // btnGetElementByTagName
+            // 
+            this.btnGetElementByTagName.Location = new System.Drawing.Point(673, 26);
+            this.btnGetElementByTagName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGetElementByTagName.Name = "btnGetElementByTagName";
+            this.btnGetElementByTagName.Size = new System.Drawing.Size(111, 73);
+            this.btnGetElementByTagName.TabIndex = 8;
+            this.btnGetElementByTagName.Text = "Get Element By Tag Name";
+            this.btnGetElementByTagName.UseVisualStyleBackColor = true;
+            this.btnGetElementByTagName.Click += new System.EventHandler(this.btnGetElementByTagName_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(145, 151);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(552, 333);
+            this.txtResults.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 521);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.btnGetElementByTagName);
+            this.Controls.Add(this.btnFindingChildren);
+            this.Controls.Add(this.btnInnerText);
+            this.Controls.Add(this.btnReadXMLValues);
+            this.Controls.Add(this.btnReadXMLFile);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Set output as a console application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadXMLFile;
+        private System.Windows.Forms.Button btnReadXMLValues;
+        private System.Windows.Forms.Button btnInnerText;
+        private System.Windows.Forms.Button btnFindingChildren;
+        private System.Windows.Forms.Button btnGetElementByTagName;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ XMLParsing/XMLParsing/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace XMLParsing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+      
+
+    
+        private void btnReadXMLFile_Click(object sender, EventArgs e)
+        {
+            // make life easy for now, put this file in the debug directory
+            StringBuilder results = new StringBuilder();
+            string sStartupPath = Application.StartupPath + "\\StockOutput.xml";
+            if (File.Exists(sStartupPath))
+            {
+                XmlTextReader textReader = new XmlTextReader(sStartupPath);
+                textReader.Read();
+                // Check to see that you have a valid node
+                while (textReader.Read())
+                {
+                    // Move to fist element
+                    textReader.MoveToElement();
+                    Console.WriteLine("XmlTextReader Properties Test");
+                    results.AppendLine("XmlTextReader Properties Test");
+                    Console.WriteLine("===================");
+                    results.AppendLine("===================");
+                    Console.WriteLine("===================");
+                    results.AppendLine("===================");
+
+                    // Read this element's properties and display them on console
+
+                    System.Console.WriteLine("Name:" + textReader.Name);
+                    results.AppendLine("Name:" + textReader.Name);
+
+                    Console.WriteLine("Base URI:" + textReader.BaseURI);
+                    results.AppendLine("Base URI:" + textReader.BaseURI);
+
+                    Console.WriteLine("Local Name:" + textReader.LocalName);
+                    results.AppendLine("Local Name:" + textReader.LocalName);
+
+                    Console.WriteLine("Attribute Count:" + textReader.AttributeCount.ToString());
+                    results.AppendLine("Attribute Count:" + textReader.AttributeCount.ToString());
+
+                    Console.WriteLine("Depth:" + textReader.Depth.ToString());
+                    results.AppendLine("Depth:" + textReader.Depth.ToString());
+
+                    Console.WriteLine("Line Number:" + textReader.LineNumber.ToString());
+                    results.AppendLine("Line Number:" + textReader.LineNumber.ToString());
+
+                    Console.WriteLine("Node Type:" + textReader.NodeType.ToString());
+                    results.AppendLine("Node Type:" + textReader.NodeType.ToString());
+
+                    Console.WriteLine("Attribute Count:" + textReader.Value.ToString());
+                    results.AppendLine("Attribute Count:" + textReader.Value.ToString());
+                   
+                }
+                txtResults.Text = results.ToString();
+            }
+            else
+                Console.Write("File does not exist");
+        }
+
+        private void btnReadXMLValues_Click(object sender, EventArgs e)
+        {
+
+         
+            string sStartupPath = Application.StartupPath + "\\StockOutput.xml";
+            XmlTextReader objXMLTextReader = new XmlTextReader(sStartupPath);
+            string sName = "";
+            StringBuilder results = new StringBuilder();
+            while (objXMLTextReader.Read())
+            {
+                switch (objXMLTextReader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        sName = objXMLTextReader.Name;
+                        break;
+                    case XmlNodeType.Text:                    
+                        Console.WriteLine(sName + "-" + objXMLTextReader.Value);
+                        results.AppendLine(sName + "-" + objXMLTextReader.Value);
+                        break;
+                       
+                }
+            }
+            txtResults.Text = results.ToString();
+
+        }
+
+        private void btnInnerText_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\StockOutput.xml";
+            XmlDocument xmlDoc = new XmlDocument();
+            StringBuilder results = new StringBuilder();
+
+            if (File.Exists(strFilename))
+            {
+                xmlDoc.Load(strFilename);
+                XmlElement elm = xmlDoc.DocumentElement;
+                Console.WriteLine("{0}", elm.InnerText);
+                results.AppendLine(elm.InnerText);
+                Console.WriteLine("{0}", elm.OuterXml);
+                results.AppendLine(elm.OuterXml);
+                Console.WriteLine("{0}", elm.InnerXml);
+                results.AppendLine(elm.InnerXml);
+            }
+            else
+                Console.WriteLine("The file {0} could not be located",
+                                  strFilename);
+            txtResults.Text = results.ToString();
+        }
+
+        private void btnFindingChildren_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\StockOutput.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+           
+            xmlDoc.Load(strFilename);
+            XmlElement elm = xmlDoc.DocumentElement;
+            XmlNodeList stockInfo = elm.ChildNodes;
+
+            for (int iCount = 0; iCount < stockInfo.Count; iCount++)
+            {
+                Console.WriteLine(stockInfo[0].Name);
+                results.AppendLine(stockInfo[0].Name);
+                Console.WriteLine(stockInfo[0].FirstChild.Name + " - " + stockInfo[0].FirstChild.InnerText);
+                results.AppendLine(stockInfo[0].FirstChild.Name + " - " + stockInfo[0].FirstChild.InnerText);
+                Console.WriteLine("First Child Has children - " + stockInfo[0].FirstChild.HasChildNodes.ToString());
+                results.AppendLine("First Child Has children - " + stockInfo[0].FirstChild.HasChildNodes.ToString());
+                XmlNodeList stockProperties = stockInfo[0].ChildNodes;
+
+                for (int jCount = 0;jCount < stockProperties.Count; jCount ++)
+                {
+                    Console.WriteLine(stockProperties[jCount].Name + " - " + stockProperties[jCount].InnerText);
+                    results.AppendLine(stockProperties[jCount].Name + " - " + stockProperties[jCount].InnerText);
+                }
+
+            }
+
+            txtResults.Text = results.ToString();
+        }
+
+        private void btnGetElementByTagName_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\MultipleStockOutput.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            xmlDoc.Load(strFilename);
+            XmlElement elm = xmlDoc.DocumentElement;
+            XmlNodeList stocks = xmlDoc.GetElementsByTagName("Symbol");
+
+            // Now you can check each node of the list
+            foreach (XmlNode node in stocks)
+            {
+                Console.WriteLine(node.InnerText);
+                results.AppendLine(node.InnerText);
+            }
+            txtResults.Text = results.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
