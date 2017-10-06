8013a8b464eb70a3df93dda4d4b7bfb19fc24a7d
Button IV <wjbuttoniv@labs.neit.edu>
2017-10-05 22:07:07 -0400

    Lab 1 Pt1, Finished up to retrieving Attributes

---
+++ Lab1pt1/Lab1pt1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Lab1pt1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parseContactInfo = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // parseContactInfo
+            // 
+            this.parseContactInfo.Location = new System.Drawing.Point(75, 27);
+            this.parseContactInfo.Name = "parseContactInfo";
+            this.parseContactInfo.Size = new System.Drawing.Size(132, 23);
+            this.parseContactInfo.TabIndex = 0;
+            this.parseContactInfo.Text = "Parse Contact Info";
+            this.parseContactInfo.UseVisualStyleBackColor = true;
+            this.parseContactInfo.Click += new System.EventHandler(this.parseContactInfo_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Location = new System.Drawing.Point(11, 55);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(2);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ReadOnly = true;
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(262, 196);
+            this.txtResults.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.parseContactInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button parseContactInfo;
+        private System.Windows.Forms.TextBox txtResults;
+    }
+}
+
---
+++ Lab1pt1/Lab1pt1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Lab1pt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void parseContactInfo_Click(object sender, EventArgs e)
+        {
+            string strFilename = Application.StartupPath + "\\Contacts.xml";
+            StringBuilder results = new StringBuilder();
+            XmlDocument xmlDoc = new XmlDocument();
+
+
+            xmlDoc.Load(strFilename);
+            XmlElement elm = xmlDoc.DocumentElement;
+            XmlNodeList contacts = xmlDoc.GetElementsByTagName("Contact");
+            XmlNodeList contactslist = xmlDoc.GetElementsByTagName("ContactsList");
+            
+
+            foreach (XmlNode node in contactslist)
+            {
+                string version = node["VersionNumber"].InnerText;
+                Console.WriteLine(version);
+                results.AppendLine("Version Number: " + version);
+            }
+            
+            
+            foreach (XmlNode node in contacts)
+            {
+               
+                string fname = node["FirstName"].InnerText;
+                string lname = node["LastName"].InnerText;
+                string pnum = node["PhoneNumber"].InnerText;
+                string fnum = node["FaxNumber"].InnerText;
+                string email = node["EmailAddress"].InnerText;
+                
+
+
+                Console.WriteLine(fname);
+                Console.WriteLine(lname);
+                Console.WriteLine(pnum);
+                Console.WriteLine(fnum);
+                Console.WriteLine(email);
+                results.AppendLine(fname);
+                results.AppendLine(lname);
+                results.AppendLine(pnum);
+                results.AppendLine(fnum);
+                results.AppendLine(email);
+
+                results.AppendLine(Environment.NewLine); 
+            }
+
+            
+            txtResults.Text = results.ToString();
+
+        }
+    }
+ }
+
